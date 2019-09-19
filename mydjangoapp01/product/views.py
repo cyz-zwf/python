82fa418df0f9ff7c45363ea3f9ac79ced5cc9966
cyz <cyz@tedu.cn>
2019-09-19 14:54:38 +0800

    服务器端读取 GET请求url中的查询字符串数据 req.GET['kw']

--- mydjangoapp01/product/views.py
+++ mydjangoapp01/product/views.py
@@ -1,11 +1,23 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+import json   #引入json模块
 
 # Create your views here.
 
 # 路由处理函数  称为MTV中的V
 def doProductList(req):
-    res = HttpResponse('这是商品列表<hr>')
+    '''
+    print(req.GET)         #<QueryDict: {'kw': ['dell'], 'pno': ['3']}>
+    print(type(req.GET))   #<class 'django.http.request.QueryDict'>
+    print(req.GET['kw'])   #dell
+    print(req.GET['pno'])  #3
+    print(req.GET['age'])  #报错
+    '''
+
+    kw = req.GET['kw']
+    pno = req.GET['pno']
+
+    res = HttpResponse('这是商品列表<hr>查询关键字为 :%s<br> 显示字符串位 %s' %(kw,pno))
     return res
 
 def doProductDetail(req):
