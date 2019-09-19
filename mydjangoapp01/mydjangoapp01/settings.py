da94a73db7f32514e59130c8144bfe77c17a0745
cyz <cyz@tedu.cn>
2019-09-19 18:31:04 +0800

    ORM技术

--- mydjangoapp01/mydjangoapp01/settings.py
+++ mydjangoapp01/mydjangoapp01/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'mydjangoapp01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mfresh',
+        'HOST':'127.0.0.1',
+        'POST':'3306',
+        'USER':'root',
+        'PASSWORD':''
     }
 }
 
